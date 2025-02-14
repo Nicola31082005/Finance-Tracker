baa8f0e25504c54da006987ebdccd50f09409efd
Nicola31082005 <nikule512@gmail.com>
2025-02-14 02:23:02 +0200

    Revert "Remove the whole db"
    This reverts commit 7d1e1c42c22c752875ba2984c31bc013d119d0d0.

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,33 +1,33 @@
-// import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
-// const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
+const SECRET = process.env.JWT_SECRET || 'BASICSECRET';
 
-// export const authMiddleware = (req, res, next) => {
-//     const token = req.cookies['auth'];
+export const authMiddleware = (req, res, next) => {
+    const token = req.cookies['auth'];
 
-//     if (!token) {
-//         return next();
-//     }
+    if (!token) {
+        return next();
+    }
 
-//     try {
-//         const decodedToken = jwt.verify(token, SECRET);
+    try {
+        const decodedToken = jwt.verify(token, SECRET);
 
-//         req.user = decodedToken;
-//         res.locals.user = decodedToken;
+        req.user = decodedToken;
+        res.locals.user = decodedToken;
+        
+        next();
+    } catch(err) {
+        res.setError('Invalid Authentication!');
+        res.clearCookie('auth');
+        res.redirect('/auth/login');
+    }
+};
 
-//         next();
-//     } catch(err) {
-//         res.setError('Invalid Authentication!');
-//         res.clearCookie('auth');
-//         res.redirect('/auth/login');
-//     }
-// };
+export const isAuth = (req, res, next) => {
+    if (!req.user) {
+        res.setError('You must be logged in in order to do that!')
+        return res.redirect('/auth/login');
+    } 
 
-// export const isAuth = (req, res, next) => {
-//     if (!req.user) {
-//         res.setError('You must be logged in in order to do that!')
-//         return res.redirect('/auth/login');
-//     }
-
-//     next();
-// }
+    next();
+}
